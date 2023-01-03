293f7720d5e7172b046017420990b0b0f61af2a5
Emily Dixon <edixon@mux.com>
2023-01-03 15:43:43 -0800

    Initial Commit

---
+++ Mux Spaces UIKit ExampleUITests/Mux_Spaces_UIKit_ExampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Mux_Spaces_UIKit_ExampleUITestsLaunchTests.swift
+//  Mux Spaces UIKit ExampleUITests
+//
+//  Created by Emily Dixon on 1/3/23.
+//
+
+import XCTest
+
+final class Mux_Spaces_UIKit_ExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
